8e339e097edd438c2acd72be67935918738b8e7a
MrRottweiler <WhyNotVOL4@gmail.com>
2025-06-30 22:49:53 +0800

    Delete AdminRecepStudent_src directory

--- AdminRecepStudent_src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Ensure that Swing operations are performed on the Event Dispatch Thread (EDT)
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            loginPageTest loginPage = new loginPageTest();
-        });
-    }
-}
